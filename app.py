ff9416330c6ea744867225caf708421104730275
Edouard Bourque <edbourque0@gmail.com>
2024-02-09 12:03:32 -0500

    Code optimization Pylint

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import random
+import time
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import requests
-import random
-import time
 
 
 app = Flask(__name__)
@@ -22,22 +22,22 @@ tricheur = '' #Variable pour stocker le nom du tricheur temporairement
 round_number = 0 #Nombre de round jouées dans la partie
 joueurs_en_attente = []
 
-#Va chercher la question sur l'API de opentdb
+
 def obtenir_question(max_retries=5):
+    #Va chercher la question sur l'API de opentdb
     url = "https://opentdb.com/api.php?amount=1&type=multiple"
     for attempt in range(max_retries):
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             return data['results'][0]
-        else:
-            print(f"Tentative {attempt + 1} échouée, statut {response.status_code}. Réessai dans 1 seconde...")
-            time.sleep(1)
+        time.sleep(1)
     return None
 
-#Assigne les roles aux joueurs
+
 def assigner_roles():
-    global roles
+    #Assigne les roles aux joueurs
+    global tricheur
     temp_players = joueurs[:]
     tricheur = random.choice(temp_players)
     roles[tricheur] = 'Tricheur'
@@ -47,36 +47,33 @@ def assigner_roles():
     temp_players.remove(lecteur)
     for joueur in temp_players:
         roles[joueur] = 'Joueur'
-    
 
-#Fonction pour reseter les paramêtres pour pouvoir jouer une nouvelle partie
+
 def reset_param():
-        if len(joueurs_en_attente) == 0:
-            global joueurs, roles, partie_demarree, question_actuelle, points, tricheur, tricheur_revele, round_number
-            joueurs = []
-            roles = {}
-            partie_demarree = False
-            question_actuelle = None
-            tricheur_revele = False
-            tricheur = ''
-            init_points()
-            round_number = 0
+    #Fonction pour reseter les paramêtres pour pouvoir jouer une nouvelle partie
+    if len(joueurs_en_attente) == 0:
+        global joueurs, roles, partie_demarree, question_actuelle, tricheur, tricheur_revele, round_number
+        joueurs = []
+        roles = {}
+        partie_demarree = False
+        question_actuelle = None
+        tricheur_revele = False
+        tricheur = ''
+        init_points()
+        round_number = 0
 
-#Fonction pour retourner un joueur
 def retourner(role_retourne, joueur_retourne):
-    global retourne
+    #Fonction pour retourner un joueur
     retourne[joueur_retourne] = role_retourne
     socketio.emit('Joueur retourne', {'joueur': joueur_retourne, 'role': role_retourne})
 
-#Assigner 0 points à tout les joueurs
 def init_points():
-        global roles, points
-        for user in roles:
-            points[user] = 0
+    #Assigner 0 points à tout les joueurs
+    for user in roles:
+        points[user] = 0
 
-#Assigner les points de la partie aux joueurs
 def assigner_points():
-    global retourne, points, joueurs
+    #Assigner les points de la partie aux joueurs
     for user in roles:
         if roles[user] == 'Lecteur' and len(retourne) == len(joueurs)-2:
             points[user] += len(joueurs)-1
@@ -90,30 +87,28 @@ def assigner_points():
             points[user] += 0
     print(points)
 
-#Vérifier qu'un nouveau joueur n'est pas ajouté lors d'une partie
 def verifier_nouveau_joueur(nom):
+    #Vérifier qu'un nouveau joueur n'est pas ajouté lors d'une partie
     if round_number > 0 and nom not in joueurs:
         return False
-    else:
-        return True
-    
+    return True
+
 def attendre(nom):
+    #Crée une liste des joueurs en attente
     if nom not in joueurs_en_attente:
         joueurs_en_attente.append(nom)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    global tricheur_revele
     if request.method == 'POST':
         nom_joueur = request.form.get('nom')
-        if verifier_nouveau_joueur(nom_joueur) == True:
+        if verifier_nouveau_joueur(nom_joueur) is True:
             if nom_joueur and nom_joueur not in joueurs:
                 joueurs.append(nom_joueur)
                 session['nom'] = nom_joueur
             return redirect(url_for('salle_attente'))
-        else:
-            socketio.emit('Nouvelle partie')
-            return render_template('index.html', message=True)
+        socketio.emit('Nouvelle partie')
+        return render_template('index.html', message=True)
     return render_template('index.html', nbattente=len(joueurs_en_attente), round_number=round_number)
 
 @app.route('/reset', methods=['POST'])
@@ -130,11 +125,11 @@ def salle_attente():
     if partie_demarree:
         return redirect(url_for('partie'))
     premier_joueur = joueurs[0] if joueurs else None
-    return render_template('salle_attente.html', joueurs=joueurs, joueur_session=session.get('nom'), premier_joueur=premier_joueur, joueurs_en_attente=len(joueurs_en_attente), nbjoueurs=len(joueurs))
+    return render_template('salle_attente.html', joueurs=joueurs, joueur_session=session.get('nom'),premier_joueur=premier_joueur, joueurs_en_attente=len(joueurs_en_attente), nbjoueurs=len(joueurs))
 
 @app.route('/lancer_jeu', methods=['POST'])
 def lancer_jeu():
-    global partie_demarree, question_actuelle, retourne
+    global partie_demarree, question_actuelle
     if 'nom' in session and joueurs and session['nom'] == joueurs[0]:
         question_actuelle = obtenir_question()
         if len(joueurs_en_attente) == len(joueurs):
@@ -143,16 +138,14 @@ def lancer_jeu():
                 socketio.emit('Partie commencée')
                 assigner_roles()
                 return redirect(url_for('partie'))
-            else:
-                partie_demarree = False
-                return "Erreur lors de la récupération de la question, veuillez réessayer.", 500
+            partie_demarree = False
+            return "Erreur lors de la récupération de la question, veuillez réessayer.", 500
         else:
             return redirect(url_for('salle_attente'))
     return redirect(url_for('salle_attente'))
 
 @app.route('/partie')
 def partie():
-    global joueurs_en_attente
     joueurs_en_attente.clear()
     if tricheur_revele:
         return redirect(url_for('resultat'))
@@ -174,7 +167,7 @@ def nouvelle_question():
 
 @app.route('/verifier_joueur/<joueur>', methods=['POST'])
 def verifier_joueur(joueur):
-    global points, tricheur_revele, tricheur, round_number
+    global tricheur_revele, round_number, tricheur
     if tricheur_revele:  # Si le tricheur a déjà été révélé, ne rien faire
         return jsonify({"redirect": url_for('resultat')}), 200
     if roles[joueur] == 'Tricheur':
