c2b004cc8dc5fc8a021cd609bf125488d79a8177
Edouard Bourque <edbourque0@gmail.com>
2024-02-13 17:31:27 -0500

    allow production

--- app.py
+++ app.py
@@ -203,4 +203,4 @@ def handle_connect():
     
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=2827)
+    socketio.run(app, host='0.0.0.0', port=2827, allow_unsafe_werkzeug=True)
